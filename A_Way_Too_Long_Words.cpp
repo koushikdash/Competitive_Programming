4a252f91982bbda6daed72f083b7e6da176dd928
koushikdash <kaushikdas5351@gmail.com>
2024-10-24 23:23:25 +0600

    71A Solved

--- A_Way_Too_Long_Words.cpp
+++ A_Way_Too_Long_Words.cpp
@@ -1,8 +1,21 @@
-#include <iostream.h>
+#include <iostream>
+#include <cstring>
 using namespace std;
 int main()
 {
-    int a;
+    int n;
+    cin>>n;
+    //char word[100];
+    string word;
+    while (n--)
+    {
+        cin >> word;
+        int String_length = word.length();
+        if(String_length>10){
+            cout<<word[0]<<String_length-2<<word[String_length-1]<<endl;
+        }
+        else cout<<word<<endl;
+    }
 
     return 0;
 }
