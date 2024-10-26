232b7b1041110553dfd38842ba430406064a0254
koushikdash <kaushikdas5351@gmail.com>
2024-10-27 00:36:10 +0600

    all integers added at one

--- A_Team.cpp
+++ A_Team.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int count = 0;
+    int n,i,petya,vasya,tonya,count = 0;
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        int petya, vasya, tonya;
         cin >> petya >> vasya >> tonya;
         if (petya + vasya + tonya >= 2)
             count++;
